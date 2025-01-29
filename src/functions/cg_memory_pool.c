cf5d9e7f8db452670c0ba434465565465bdb5ebb
Soulgamer <Sosoulgamer@outlook.com>
2025-01-29 22:21:21 +0800

    修改：     src/functions/cg_memory_pool.c

--- src/functions/cg_memory_pool.c
+++ src/functions/cg_memory_pool.c
@@ -278,6 +278,7 @@ void *cg_realloc_memory(cg_memory_pool_var_t *p_var, void *memory_addr, size_t s
 	}
 	if (size == 0) {
 		cg_free_memory(p_var, memory_addr);
+		
 		return NULL;
 	}
 	return memory_addr;
