7361ac26da8eed911b1dd854f60eeb0f5d3a581d
Soulgamer <Sosoulgamer@outlook.com>
2025-07-05 00:53:26 +0800

    检查拼写

--- src/functions/cg_physical_device.h
+++ src/functions/cg_physical_device.h
@@ -6,9 +6,9 @@
 #endif // __linux
 
 // 创建Vulkan物理设备列表
-bool cg_enumerate_physical_device(cg_var_t *p_var, uint32_t *p_physical_device_count, VkPhysicalDevice *available_physical_device_arry);
+bool cg_enumerate_physical_device(cg_var_t *p_var, uint32_t *p_physical_device_count, VkPhysicalDevice *available_physical_device_array);
 
 // 选择想要的Vulkan物理设备
-bool cg_select_physical_device(cg_var_t *p_var, uint32_t *p_physical_device_count, VkPhysicalDevice *available_physical_device_arry, VkPhysicalDevice *p_physical_device);
+bool cg_select_physical_device(cg_var_t *p_var, uint32_t *p_physical_device_count, VkPhysicalDevice *available_physical_device_array, VkPhysicalDevice *p_physical_device);
 
 #endif // CG_PHYSICAL_DEVICE_H 1
