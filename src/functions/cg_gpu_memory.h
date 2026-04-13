78b081e6d4b26c0698d596661b3d65d55f86315f
Soulgamer <SOsoulgamer@outlook.com>
2026-04-13 11:48:44 +0800

    modified:   src/functions/cg_gpu_memory.c modified:   src/functions/cg_gpu_memory.h

--- src/functions/cg_gpu_memory.h
+++ src/functions/cg_gpu_memory.h
@@ -19,8 +19,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef CG_GPU_MEMORY_H
 #define CG_GPU_MEMORY_H 1
-#include "cg_log.h"
 #include "cg_info.h"
+#include "cg_log.h"
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -29,52 +29,5 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #define GPU_MEMORY_POOL_SIZE (4ULL * 1024 * 1024 * 1024)
 #define MAX_FREE_GPU_MEM_NODE_COUNT (4U * 1024)
 #define VK_DEVICE_ADDR_NULL 0ULL
-
-// 用来记录GPU内存池信息(侵入式内存池,依赖VulkanAPI),可以根据情况再创建各自独立的多个内存池
-typedef struct cg_gpu_memory_var {
-	// 内存池,内存池开始地址
-	VkDeviceAddress memory_pool;
-	// 内存池总大小
-	VkDeviceSize size;
-	// 内存池剩余可用大小
-	VkDeviceSize free_size;
-	// 内存块数量,可能包括空闲内存块
-	uint32_t memory_count;
-	// 保存排在最后的内存块的大小
-	VkDeviceSize last_memory_size;
-	// 保存排在最后的内存块的尾地址
-	VkDeviceAddress last_memory_end_addr;
-	// 空闲内存块信息节点数量
-	uint32_t free_memory_node_count;
-	// 空闲内存块信息节点指针的列表
-	VkDeviceAddress *free_memory_node_addr_array;
-	// Vulkan 显存句柄
-	VkDeviceMemory vk_device_memory;
-} cg_gpu_memory_var_t;
-
-// 记录GPU内存块的信息的节点,节点本身位置在内存块的前面
-typedef struct cg_gpu_memory_node {
-	// 内存块地址
-	VkDeviceAddress memory_addr;
-	// 内存块大小(不包含内存信息节点本身)
-	VkDeviceSize size;
-	// 表示内存块是否被使用
-	bool is_used;
-	// 记录上一个内存块信息节点的地址
-	VkDeviceAddress prev_memory_node_addr;
-} cg_gpu_memory_node_t;
-
-/*创建gpu内存池*/
-bool cg_create_gpu_memory(cg_gpu_memory_var_t *p_mp, cg_var_t *vk_var);
-
-// 分配gpu内存
-VkDeviceAddress cg_alloc_gpu_memory(cg_gpu_memory_var_t *p_mp, size_t size);
-
-typedef struct cg_gpu_var {
-	VkBufferCreateInfo buffer_create_info;
-	PFN_vkCreateBuffer ceate_buffer;
-} cg_gpu_var_t;
-
-bool cg_gpu_create_vk_buffer(cg_var_t *p_var, cg_gpu_var_t *p_cg_gpu_var_t);
 
 #endif // CG_GPU_MEMORY_H 1
