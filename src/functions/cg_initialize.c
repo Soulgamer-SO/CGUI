9a858b1b0359b745771b45c430aaec1a5cffcbab
Soulgamer-SO <SOsoulgamerSO@outlook.com>
2026-08-30 03:15:18 +0800

    modified:   src/functions/cg_initialize.c

--- src/functions/cg_initialize.c
+++ src/functions/cg_initialize.c
@@ -50,6 +50,7 @@ bool cg_initialize_var(cg_info_t *p_info) {
         PRINT_ERROR("load library fail!\n");
         return false;
     }
+
     if (cg_create_instance(p_info, &p_info->instance.vk_instance) == false) {
         PRINT_ERROR("create instance fail!\n");
         return false;
@@ -65,6 +66,7 @@ bool cg_initialize_var(cg_info_t *p_info) {
         PRINT_ERROR("enumerate physical device fail!\n");
         return false;
     }
+
     p_info->physical_device.available_physical_device_array = (VkPhysicalDevice *)cg_alloc_memory(p_info->p_memory_pool, p_info->physical_device.physical_device_count * sizeof(VkPhysicalDevice));
     if (p_info->physical_device.available_physical_device_array == nullptr) {
         PRINT_ERROR("create available_handle_device_array fail!\n");
@@ -75,10 +77,12 @@ bool cg_initialize_var(cg_info_t *p_info) {
         PRINT_ERROR("enumerate physical device fail!\n");
         return false;
     }
+
     if (cg_select_physical_device(p_info, &p_info->physical_device.physical_device_count, &p_info->physical_device.available_physical_device_array[0], &p_info->physical_device.physical_device) == false) {
         PRINT_ERROR("select physical device fail!\n");
         return false;
     }
+
     if (cg_create_logic_device(p_info, &p_info->logic_device.vk_logic_device) == false) {
         PRINT_ERROR("create logic device fail!\n");
         return false;
@@ -103,21 +107,6 @@ bool cg_initialize_var(cg_info_t *p_info) {
         cg_reset_command_buffer(p_info, p_info->command_pool.command_buffer_array[0], VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT);
         cg_reset_command_pool(p_info, p_info->command_pool.command_pool, VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT);
     }
-
-    /* p_info->sync.semaphore_count = 1;
-    p_info->sync.semaphore_array = (VkSemaphore *)cg_alloc_memory(p_info->p_memory_pool,p_info->sync.semaphore_count * sizeof(VkSemaphore));
-    if (nullptr != p_info->sync.semaphore_array)
-    {
-        create_semaphore(p_info);
-    }
-    p_info->sync.fence_count = 1;
-    p_info->sync.fence_array = (VkFence *)cg_alloc_memory(p_info->p_memory_pool,p_info->sync.fence_count * sizeof(VkFence));
-    if (nullptr != p_info->sync.fence_array)
-    {
-        create_fence(p_info);
-        wait_for_fences(p_info);
-        reset_for_fences(p_info);
-    } */
 
     if (cg_create_swapchain_resources(p_info) == false) {
         PRINT_ERROR("create swapchain fail!\n");
@@ -149,18 +138,6 @@ bool cg_initialize_var(cg_info_t *p_info) {
         PRINT_ERROR("create synchronization objects fail!\n");
         return false;
     }
-
-    /* p_info->sync.wait_semaphore_count = 1;
-    p_info->sync.wait_semaphore_array = (VkSemaphore *)cg_alloc_memory(p_info->p_memory_pool,p_info->sync.wait_semaphore_count * sizeof(VkSemaphore));
-    p_info->sync.semaphore_pipeline_stage_count = 1;
-    p_info->sync.semaphore_pipeline_stage_array = (VkPipelineStageFlags *)cg_alloc_memory(p_info->p_memory_pool,p_info->sync.semaphore_pipeline_stage_count * sizeof(VkPipelineStageFlags));
-    p_info->sync.semaphore_pipeline_stage_array[0] = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
-    if (nullptr != p_info->sync.semaphore_array &&
-        nullptr != p_info->sync.semaphore_pipeline_stage_array &&
-        nullptr != p_info->sync.fence_array)
-    {
-        submit_command_buff_to_queue(p_info);
-    } */
 
     PRINT_LOG("initialize success!\n");
     return true;
