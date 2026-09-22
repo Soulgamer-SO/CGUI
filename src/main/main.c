64f317d4c7acb88e02c1449041ba0869a8758f1c
Soulgamer <SOsoulgamer@outlook.com>
2026-09-22 18:57:22 +0800

    renamed:    src/main/cg_platform.h -> src/functions/cg_platform.h 	modified:   src/main/main.c

--- src/main/main.c
+++ src/main/main.c
@@ -22,7 +22,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "../functions/cg_initialize.h"
 #include "../functions/cg_log.h"
 #include "../functions/cg_memory.h"
-#include "cg_platform.h"
+#include "../functions/cg_platform.h"
 #define CG_MEMORY_POOL_SIZE (4ULL * 1024 * 1024 * 1024)
 #define CG_MAX_FREE_MEM_NODE_COUNT (4U * 1024)
 
